97b5cb8b6873520e1ea0188bf71186929d4d115c
深绘里 <142601975+Sehuri@users.noreply.github.com>
2026-08-28 10:21:59 +0800

    Configure CARTO basemap API key

--- assets/app.js
+++ assets/app.js
@@ -206,7 +206,7 @@
       worldCopyJump: true
     });
 
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+    L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?key=cb1_2er4_1_991de9fa689e4c42aeee39c4", {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; CARTO',
       subdomains: "abcd",
       maxZoom: 19
